9b8da32683de2c7cc82a6d5b7e4a1b5958814cd5
a3at28 <a3at28@gmail.com>
2021-02-24 19:02:48 +0300

    pom.xml add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int percent = 3;
+        int depositAmount = 6000;
+        int income = percent * depositAmount / 100;
+        System.out.println(income);
+    }
 }
